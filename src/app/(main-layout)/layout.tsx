3db2411bb3eb9959f1381ab2cafb4e464bdf33d4
DarellYuhu <darell.mona242@gmail.com>
2025-06-19 13:34:39 +0700

    feat: logout button on header chore: add missing variable to .env.example

--- src/app/(main-layout)/layout.tsx
+++ src/app/(main-layout)/layout.tsx
@@ -17,6 +17,10 @@ import {
 import { SessionProvider } from "next-auth/react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import AppSidebar from "@/components/sidebar/AppSidebar";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,13 +36,14 @@ export default function MainLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
         <SidebarProvider defaultOpen={true}>
           <AppSidebar />
           <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+            <header className="flex h-16 w-full justify-between shrink-0 pr-4 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
               <div className="flex items-center gap-2 px-4">
                 <SidebarTrigger className="-ml-1" />
                 <Separator orientation="vertical" className="mr-2 h-4" />
@@ -52,6 +57,15 @@ export default function MainLayout({
                   </BreadcrumbList>
                 </Breadcrumb>
               </div>
+              <Button
+                onClick={() =>
+                  signOut({ redirect: false }).then(() => {
+                    router.push("/sign-in");
+                  })
+                }
+              >
+                <LogOut /> Logout
+              </Button>
             </header>
             <div className="flex flex-1 flex-col p-4">
               {children}
